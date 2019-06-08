92ccda51dac83fc8574997b554b59b97dbaef09a
wcypersonal <417177466@qq.com>
2019-06-08 17:31:38 +0800

    quick start

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,12 +1,9 @@
-'use strict';
-
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-    const { ctx } = this;
-    ctx.body = 'hi, egg';
-  }
+    async index(){
+        this.ctx.body = 'Hello'
+    }
 }
 
-module.exports = HomeController;
+module.exports = HomeController
